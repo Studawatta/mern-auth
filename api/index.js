04441bf21b39b255de27f45329489815c52b9eab
Sandun Thisara <89610546+Studawatta@users.noreply.github.com>
2024-10-06 16:12:54 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(8080, () => {
   console.log('Server is running');
